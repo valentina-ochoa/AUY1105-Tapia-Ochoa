ab301d80a77d5a9796382daab63a0a39998c2bbf
valentina-ochoa <val.ochoa@duocuc.cl>
2026-04-23 17:40:40 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "AUY1105-app-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.1.0/24"
+
+  tags = {
+    Name = "AUY1105-app-subnet"
+  }
+}
+
+resource "aws_security_group" "sg" {
+  description = "SSH access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["201.188.31.24/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "AUY1105-app-sg"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  subnet_id              = aws_subnet.subnet_public.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "AUY1105-app-ec2"
+  }
+}
