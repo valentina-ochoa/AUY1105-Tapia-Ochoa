5ae17aff1a749c5b4abe15b58a53da2d45f5eb9c
valentina-ochoa <val.ochoa@duocuc.cl>
2026-04-23 18:00:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,9 @@ resource "aws_vpc" "main" {
   }
 }
 
-# 🔐 Default SG bloqueado
+# -----------------------------
+# DEFAULT SECURITY GROUP BLOQUEADO
+# -----------------------------
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.main.id
 
@@ -123,4 +125,59 @@ resource "aws_instance" "ec2" {
   tags = {
     Name = "AUY1105-app-ec2"
   }
+}
+
+# -----------------------------
+# CLOUDWATCH LOG GROUP
+# -----------------------------
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name = "/aws/vpc/flow-logs"
+}
+
+# -----------------------------
+# IAM ROLE PARA FLOW LOGS
+# -----------------------------
+resource "aws_iam_role" "flow_logs_role" {
+  name = "flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs_policy" {
+  name = "flow-logs-policy"
+  role = aws_iam_role.flow_logs_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+# -----------------------------
+# VPC FLOW LOG (CLAVE PARA PASAR CHECKOV)
+# -----------------------------
+resource "aws_flow_log" "vpc_flow" {
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  log_destination_type = "cloud-watch-logs"
+  traffic_type         = "ALL"
+
+  iam_role_arn = aws_iam_role.flow_logs_role.arn
+  vpc_id       = aws_vpc.main.id
 }
