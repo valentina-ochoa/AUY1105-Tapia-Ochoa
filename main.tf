bd676cbe75f33b44cc80300efd4d9ce5cb01dd08
valentina-ochoa <val.ochoa@duocuc.cl>
2026-04-23 22:45:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-#############################################
-# TERRAFORM CONFIG
-#############################################
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,91 +7,58 @@ terraform {
   }
 }
 
-#############################################
-# PROVIDER
-#############################################
 provider "aws" {
   region = "us-east-1"
 }
 
-#############################################
-# DATA
-#############################################
-data "aws_caller_identity" "current" {}
-
-#############################################
-# VPC
-#############################################
 resource "aws_vpc" "main" {
   cidr_block = "10.1.0.0/16"
 
   tags = {
-    Name = "AUY1105-buymax-vpc"
+    Name = "AUY1105-app-vpc"
   }
 }
 
-#############################################
-# SUBNET PRIVADA (MEJOR PRÁCTICA)
-#############################################
-resource "aws_subnet" "subnet_private" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.1.1.0/24"
-  map_public_ip_on_launch = false
+resource "aws_subnet" "subnet_public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.1.0/24"
 
   tags = {
-    Name = "AUY1105-buymax-subnet"
+    Name = "AUY1105-app-subnet"
   }
 }
 
-#############################################
-# SECURITY GROUP (SOLO SSH SEGURO)
-#############################################
 resource "aws_security_group" "sg" {
-  name        = "AUY1105-buymax-sg"
-  description = "Allow SSH only from trusted IP"
-  vpc_id      = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
   ingress {
-    description = "SSH from trusted IP"
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["201.188.31.24/32"] # 
+    cidr_blocks = ["201.188.31.24/32"]
   }
 
   egress {
-    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-#############################################
-# EC2 SEGURA (FIX t3.micro + Ubuntu 24.04)
-#############################################
-resource "aws_instance" "ec2" {
-  ami           = "ami-0a0e5d9c7acc336f1" # Ubuntu 24.04 LTS (us-east-1)
-  instance_type = "t3.micro"
-
-  subnet_id              = aws_subnet.subnet_private.id
-  vpc_security_group_ids = [aws_security_group.sg.id]
-
-  associate_public_ip_address = false
-
-  monitoring    = true
-  ebs_optimized = true
-
-  root_block_device {
-    encrypted = true
-  }
-
-  metadata_options {
-    http_tokens = "required"
-  }
 
   tags = {
-    Name = "AUY1105-buymax-ec2"
+    Name = "AUY1105-app-sg"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  subnet_id              = aws_subnet.subnet_public.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "AUY1105-app-ec2"
   }
 }
